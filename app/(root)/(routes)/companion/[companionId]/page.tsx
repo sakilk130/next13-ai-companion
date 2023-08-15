40a037d8b1a134ae177b9bc091a19baa5fcb9168
Sakil Khan <sakilk130@gmail.com>
2023-08-15 21:52:49 +0600

    fix: companion edit page user auth check

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,3 +1,5 @@
+import { auth, redirectToSignIn } from '@clerk/nextjs';
+
 import prismadb from '@/lib/prismadb';
 import { CompanionForm } from './components/companion-form';
 
@@ -10,11 +12,17 @@ interface CompanionIdPageProps {
 const CompanionIdPage = async ({
   params: { companionId },
 }: CompanionIdPageProps) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirectToSignIn();
+  }
   const companion = await prismadb.companion.findUnique({
     where: {
+      userId,
       id: companionId,
     },
   });
+
   const categories = await prismadb.category.findMany();
 
   return (
