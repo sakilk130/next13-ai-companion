a01bf49458b5d1174bc8a4e0d0016920a5934a1b
Sakil Khan <sakilk130@gmail.com>
2023-08-15 16:00:18 +0600

    feat: root page companion list show

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,43 @@
 import { Categories } from '@/components/categories';
+import { Companions } from '@/components/companions';
 import { SearchInput } from '@/components/search-input';
 import prismadb from '@/lib/prismadb';
 
-const RootPage = async () => {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
+
+const RootPage = async ({
+  searchParams: { categoryId, name },
+}: RootPageProps) => {
   const categories = await prismadb.category.findMany();
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId,
+      name: {
+        search: name,
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
 
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   );
 };
---
+++ components/companions.tsx
@@ -0,0 +1,62 @@
+import { Companion } from '@prisma/client';
+import { MessagesSquare } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FC } from 'react';
+
+import { Card, CardFooter, CardHeader } from '@/components/ui/card';
+
+interface CompanionsProps {
+  data: (Companion & {
+    _count: {
+      messages: number;
+    };
+  })[];
+}
+
+const Companions: FC<CompanionsProps> = ({ data }) => {
+  if (data.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center p-10 space-y-3">
+        <div className="relative w-60 h-60">
+          <Image fill alt="Rmpty" className="grayscale" src="/empty.png" />
+        </div>
+        <p className="text-sm text-muted-foreground">No companions found.</p>
+      </div>
+    );
+  }
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      {data.map((companion) => (
+        <Card
+          key={companion.id}
+          className="transition border-0 cursor-pointer bg-primary/10 rounded-xl hover:opacity-75"
+        >
+          <Link href={`chat/${companion.id}`}>
+            <CardHeader className="flex items-center justify-center text-center text-muted-foreground">
+              <div className="relative w-32 h-32">
+                <Image
+                  src={companion.src || '/placeholder.svg'}
+                  alt="Companion Image"
+                  className="object-cover rounded-xl"
+                  fill
+                />
+              </div>
+              <p className="font-bold">{companion.name}</p>
+              <p className="text-xs">{companion.description}</p>
+            </CardHeader>
+            <CardFooter className="flex items-center justify-between text-xs text-muted-foreground">
+              <p className="lowercase">@{companion.userName}</p>
+              <div className="flex items-center">
+                <MessagesSquare className="w-3 h-3 mr-1" />
+                {companion._count.messages}
+              </div>
+            </CardFooter>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export { Companions };
