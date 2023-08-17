5c6c84db544d3170cff7ffd7c67ccf51c79252f1
Sakil Khan <sakilk130@gmail.com>
2023-08-18 00:31:11 +0600

    fix: pro check issue fixed

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,15 +2,17 @@ import { Menu } from 'lucide-react';
 
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Sidebar } from '@/components/sidebar';
+import { checkSubscription } from '@/lib/subscription';
 
-const MobileSidebar = () => {
+const MobileSidebar = async () => {
+  const isPro = await checkSubscription();
   return (
     <Sheet>
       <SheetTrigger className="pr-4 md:hidden">
         <Menu />
       </SheetTrigger>
       <SheetContent>
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
